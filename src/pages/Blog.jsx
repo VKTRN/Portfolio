24167c7d1d72f10f8acf8577cf8fe24a528e3102
VKTRN <vktr.ndns@gmail.com>
2022-02-14 23:20:30 +0100

    Add Blog

---
+++ src/pages/Blog.jsx
@@ -0,0 +1,90 @@
+import {useState, useRef, useEffect, useContext} from "react"
+// import '../scss/custom.scss';
+import Header from "../components/Header"
+import Footer from "../components/Footer"
+import Editor from '../components/Editor'
+
+const initial_html = "<div class='container'>A</div>\n<div class='container2'>B</div>"
+// const initial_css = ".container{\n\tbackground-color: blue;\n}"
+const initial_css = ".container{\
+                    \n\tbackground-color: white;\
+                    \n\theight: 100px;\
+                    \n\twidth: 100px;\
+                    \n\tposition: relative;\
+                    \n\ttop: 100px;\
+                    \n\tleft: 100px;\
+                    \n}\
+                    \n\
+                    \n.container2{\
+                    \n\tbackground-color: orange;\
+                    \n\theight: 100px;\
+                    \n\twidth: 100px;\
+                    \n\tposition: relative;\
+                    \n\ttop: 100px;\
+                    \n\tleft: 100px;\
+                    \n}"
+
+
+function Blog() {
+
+  const [html, setHtml] = useState(initial_html)
+  const [css, setCss] = useState(initial_css)
+
+  function getProperty(property, selector){
+    const selectorStringStart = css.indexOf(selector)
+    const selectorStringEnd = css.slice(selectorStringStart).indexOf("}")+selectorStringStart
+    const cssString = css.slice(selectorStringStart, selectorStringEnd)
+    const start = cssString.indexOf(property)+property.length+1
+    const end   = cssString.slice(start).indexOf(";") + start
+    const value = cssString.slice(start,end).replace(/\s/g, "")
+
+    return value
+  }
+
+  return (
+    <div className="Blog">
+      <Header></Header>
+
+      <div className="blog">
+        {/* <div className="pane top-pane">
+          <Editor
+            language="xml"
+            displayName="HTML"
+            value={html}
+            onChange={setHtml}
+          />
+          <Editor
+            language="css"
+            displayName="CSS"
+            value={css}
+            onChange={setCss}
+          />
+        </div>
+
+        <div className="pane" >
+          <div className="element container2" style={{ backgroundColor:getProperty("background-color", "container2"), 
+                                              height:getProperty("height", "container2"),
+                                              width:getProperty("width", "container2"),
+                                              position:getProperty("position", "container2"),
+                                              top:getProperty("top", "container2"),
+                                              left:getProperty("left", "container2"),
+                                            }}>B
+          </div>
+          <div className="element container" style={{ backgroundColor:getProperty("background-color", "container"), 
+                                              height:getProperty("height", "container"),
+                                              width:getProperty("width", "container"),
+                                              position:getProperty("position", "container"),
+                                              top:getProperty("top", "container"),
+                                              left:getProperty("left", "container"),
+                                            }}>A
+          </div>
+        </div> */}
+      </div>
+
+      <Footer></Footer>
+
+    </div>
+  );
+}
+
+export default Blog;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,80 @@
+import {useState, useRef, useEffect, useContext} from "react"
+// import '../scss/custom.scss';
+import StackItem from "../components/StackItem"
+import SocialButton from "../components/SocialButton"
+import Header from "../components/Header"
+import Footer from "../components/Footer"
+
+const text_html      = "html html html html html html html html html html html html html html html html html html html html html html html html html html "
+const text_css       = "css css css css css css css css css css css css css css css css css css css css css css css css css css css css css css css css css" 
+const text_js        = "JavaScript JavaScript JavaScript JavaScript JavaScript JavaScript JavaScript JavaScript JavaScript JavaScript JavaScript JavaScript " 
+const text_bootstrap = "bootstrap bootstrap bootstrap bootstrap bootstrap bootstrap bootstrap bootstrap bootstrap bootstrap bootstrap bootstrap bootstrap "
+
+function Home() {
+
+  const [open, set_open] = useState(Array(12))
+
+  return (
+    <div className="Home">
+      <Header></Header>
+
+      <div className="home">
+        <div className="content-container">
+
+          <div className="introduction">
+            <h1>Hi!</h1>
+            <p>
+              My name is Viktor.
+            </p>
+            <p>
+              I am a front-end developer based in Hannover, Germany.
+              <br/>
+              I love to write clean and readable code and to <a href="https://en.wikipedia.org/wiki/Don%27t_repeat_yourself">not repeat myself</a>.
+            </p>
+          </div>
+
+          <div className="tech-stack">
+            <h2>My Tech Stack</h2>
+          </div>
+
+          <h3 className="mt-5">front end</h3>
+          <div className="front-end stack">
+            <StackItem id="HTML5"      text={text_html} open={open} set_open={set_open} i={0}></StackItem>
+            <StackItem id="CSS3"       text={text_css}  open={open} set_open={set_open} i={1}></StackItem>
+            <StackItem id="JavaScript" text={text_js}   open={open} set_open={set_open} i={2}></StackItem>
+            <StackItem id="Sass"       text={text_html} open={open} set_open={set_open} i={3}></StackItem>
+            <StackItem id="React"      text={text_css}  open={open} set_open={set_open} i={4}></StackItem>
+            <StackItem id="Bootstrap"  text={text_js}   open={open} set_open={set_open} i={5}></StackItem>
+          </div>
+
+          <h3 className="mt-5">back end</h3>
+          <div className="front-end stack">
+            <StackItem id="MongoDB" text={text_html} open={open} set_open={set_open} i={6}></StackItem>
+            <StackItem id="Node"    text={text_css}  open={open} set_open={set_open} i={7}></StackItem>
+            <StackItem id="Python"  text={text_js}   open={open} set_open={set_open} i={8}></StackItem>
+          </div>
+
+          <h3 className="mt-5">dev ops</h3>
+          <div className="front-end stack">
+            <StackItem id="Bash" text={text_html} open={open} set_open={set_open} i={9}></StackItem>
+            <StackItem id="Git" text={text_html} open={open} set_open={set_open} i={10}></StackItem>
+            <StackItem id="Docker"    text={text_css}  open={open} set_open={set_open} i={11}></StackItem>
+          </div>
+
+          <div className="tech-stack">
+            <h2>Connect with me</h2>
+          </div>  
+          <div className="front-end stack socials">
+            <SocialButton id="youtube" link="https://www.youtube.com"></SocialButton>
+            <SocialButton id="twitter" link="https://www.twitter.com"></SocialButton>
+            <SocialButton id="github"  link="https://www.github.com"></SocialButton>
+          </div>
+        </div>
+      </div>
+      
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default Home;
