4a7b65d6cbb4215177305221bd4b16a0232e8d5a
VKTRN <vktr.ndns@gmail.com>
2022-02-15 15:45:15 +0100

    path to exact path

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/">
+        <Route exact path="/">
           <Home/>
         </Route>
         <Route path="/blog">
