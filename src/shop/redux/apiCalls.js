0c9d4c849d2707df49f1a310323867a729e58cf8
VKTRN <vktr.ndns@gmail.com>
2022-04-06 11:02:01 +0200

    _

--- src/shop/redux/apiCalls.js
+++ src/shop/redux/apiCalls.js
@@ -1,10 +1,12 @@
 import {loginFailure, loginStart, loginSuccess} from './userRedux'
 import { publicRequest } from '../requestMethods'
+import axios from 'axios'
 
 export const login = async (dispatch, user) => {
   dispatch(loginStart())
   try {
-    const res = await publicRequest.post('/auth/login', user)
+    // const res = await publicRequest.post('/auth/login', user)
+    const res = await axios.post('https://vktrn.com/api/auth/login', user)
     dispatch(loginSuccess(res.data))
   } catch (error) {
     dispatch(loginFailure())
