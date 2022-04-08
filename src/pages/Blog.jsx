d235a5a1f913e679034481dc345d154bf69de64c
VKTRN <vktr.ndns@gmail.com>
2022-04-08 12:31:21 +0200

    _

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -206,31 +206,31 @@ function Blog() {
     let line = getLine(1,1)
     line.addEventListener("mouseenter", () => {body_.classList.toggle("highlight")})
     line.addEventListener("mouseleave", () => {body_.classList.toggle("highlight")})
-    line.addEventListener("touchend", () => handleTouch(body_))
+    line.addEventListener("touchstart", () => handleTouch(body_))
 
 
     line = getLine(1,2)
     line.addEventListener("mouseenter", () => {container_.classList.toggle("highlight")})
     line.addEventListener("mouseleave", () => {container_.classList.toggle("highlight")})
-    line.addEventListener("touchend", () => handleTouch(container_))
+    line.addEventListener("touchstart", () => handleTouch(container_))
 
     
     line = getLine(1,3)
     line.addEventListener("mouseenter", () => {square_.classList.toggle("highlight")})
     line.addEventListener("mouseleave", () => {square_.classList.toggle("highlight")})
-    line.addEventListener("touchend", () => handleTouch(square_))
+    line.addEventListener("touchstart", () => handleTouch(square_))
 
     
     line = getLine(1,4)
     line.addEventListener("mouseenter", () => {container_.classList.toggle("highlight")})
     line.addEventListener("mouseleave", () => {container_.classList.toggle("highlight")})
-    line.addEventListener("touchend", () => handleTouch(container_))
+    line.addEventListener("touchstart", () => handleTouch(container_))
 
     
     line = getLine(1,5)
     line.addEventListener("mouseenter", () => {body_.classList.toggle("highlight")})
     line.addEventListener("mouseleave", () => {body_.classList.toggle("highlight")})
-    line.addEventListener("touchend", () => handleTouch(body_))
+    line.addEventListener("touchstart", () => handleTouch(body_))
 
 
   }, [])
