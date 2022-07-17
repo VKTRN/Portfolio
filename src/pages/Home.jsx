edff89813b1a7c4868b6200b2a7349a0dd8b5308
VKTRN <vktr.ndns@gmail.com>
2022-07-17 20:48:09 +0200

    _

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -36,25 +36,28 @@ function Home() {
           </div>
 
           <div className="about">
-            <div className="description">
-              <div className="wrapper">
-                Hello! My name is Viktor and I enjoy creating things that live on the internet. My interest in web development started back in 2012 when I decided to try editing custom Tumblr themes — turns out hacking together a custom reblog button taught me a lot about HTML & CSS!
-                Fast-forward to today, and I’ve had the privilege of working at an advertising agency, a start-up, a huge corporation, and a student-led design studio. My main focus these days is building accessible, inclusive products and digital experiences at Upstatement for a variety of clients.
-                I also recently launched a course that covers everything you need to build a web app with the Spotify API using Node & React.
+            <h1>About</h1>
+            <div className="about-wrapper">
+              <div className="description">
+                <div className="wrapper">
+                  Hello! My name is Viktor and I enjoy creating things that live on the internet. My interest in web development started back in 2012 when I decided to try editing custom Tumblr themes — turns out hacking together a custom reblog button taught me a lot about HTML & CSS!
+                  Fast-forward to today, and I've had the privilege of working at an advertising agency, a start-up, a huge corporation, and a student-led design studio. My main focus these days is building accessible, inclusive products and digital experiences at Upstatement for a variety of clients.
+                  I also recently launched a course that covers everything you need to build a web app with the Spotify API using Node & React.
+                </div>
               </div>
-            </div>
-            <div className="techstack">
-              <div className="wrapper">
-                <div className="grid">
-                  <Logo name = 'Sass'/>
-                  <Logo name = 'React'/>
-                  <Logo name = 'Bootstrap'/>
-                  <Logo name = 'CSS3'/>
-                  <Logo name = 'JavaScript'/>
-                  <Logo name = 'HTML5'/>
-                  <Logo name = 'MongoDB'/>
-                  <Logo name = 'Node'/>
-                  <Logo name = 'Docker'/>
+              <div className="techstack">
+                <div className="wrapper">
+                  <div className="grid">
+                    <Logo name = 'Sass'/>
+                    <Logo name = 'React'/>
+                    <Logo name = 'Bootstrap'/>
+                    <Logo name = 'CSS3'/>
+                    <Logo name = 'JavaScript'/>
+                    <Logo name = 'HTML5'/>
+                    <Logo name = 'MongoDB'/>
+                    <Logo name = 'Node'/>
+                    <Logo name = 'Docker'/>
+                  </div>
                 </div>
               </div>
             </div>
