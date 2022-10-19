ab124acf6c4147dcbb22d40e931dcb39d56f4d64
VKTRN <vktr.ndns@gmail.com>
2022-10-19 12:44:12 +0200

    _

--- src/components/Home/Project.jsx
+++ src/components/Home/Project.jsx
@@ -9,23 +9,25 @@ function Project({project}){
     <div className="project">
 
       <div className = 'content dark-surface'>
-  
-        <h2 className = 'title'>{project.title}</h2>
-        
-        <div className = 'company'>
-          <h3 >@{project.company}</h3>
-          <div className = 'flex column jc-c'>
-            <span>{project.date}</span>
+
+        <div>
+          <div className = 'heading'>
+            <span className = 'title'>{project.title}</span>
+            <span className = 'company'>at {project.company}</span>
           </div>
+          
+            <div className = 'date flex column jc-c'>
+              <span>{project.date}</span>
+            </div>
+
+          <h4 className = 'project-name'>{project.project}</h4>
+
+          <ul className = 'bullets'>
+            {project.bullets.map((bullet, index) => {
+              return <li key = {index}>{bullet}</li>
+            })}
+          </ul>
         </div>
-
-        <h4 className = 'project-name'>{project.project}</h4>
-
-        <ul className = 'bullets'>
-          {project.bullets.map((bullet, index) => {
-            return <li key = {index}>{bullet}</li>
-          })}
-        </ul>
 
         <div className = 'tech-stack'>
           {project.techStack.map((tech, index) => {
@@ -40,6 +42,7 @@ function Project({project}){
               </div>)
           })}
         </div>
+      
       </div>
     </div>
   )
